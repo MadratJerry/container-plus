fb1e48be0f2fdb70e402e2edba3e2f189f09d86c
Yotau Hong <crazymousethief@outlook.com>
2018-02-08 00:34:20 +0800

    ✨ Implement the basic switch
    Set printWidth to 120.

--- context.js
+++ context.js
@@ -1,11 +1,19 @@
-;(async function() {
-  const list = document.getElementById('container-list')
+class ContainerPlus {
+  constructor() {
+    this.init()
+  }
 
-  if (browser.contextualIdentities === undefined) {
-    list.innerText =
-      'browser.contextualIdentities not available. Check that the privacy.userContext.enabled pref is set to true, and reload the add-on.'
-  } else {
-    let identities = await browser.contextualIdentities.query({})
+  async init() {
+    const list = document.getElementById('container-list')
+
+    if (!browser.contextualIdentities) {
+      list.innerText =
+        'browser.contextualIdentities not available. Check that the privacy.userContext.enabled pref is set to true, and reload the add-on.'
+      return
+    }
+
+    this.identities = await browser.contextualIdentities.query({})
+    const { identities, handleClick } = this
 
     if (!identities.length) {
       list.innerText = 'No identities returned from the API.'
@@ -15,14 +23,32 @@
     const ul = document.createElement('ul')
     for (let identity of identities) {
       const li = document.createElement('li')
-      li.innerHTML = `<div class="usercontext-icon"
-      data-identity-icon="${identity.icon}"
-      data-identity-color="${identity.color}"></div>
-      <div class="container-name truncate-text">${identity.name}</div>`
-      console.log(identity.icon)
-      console.log(identity.color)
+      li.onclick = () => handleClick(identity)
+      li.innerHTML = `<div
+        class="usercontext-icon"
+        data-identity-icon="${identity.icon}"
+        data-identity-color="${identity.color}"></div>
+        <div class="container-name truncate-text">${identity.name}</div>`
+      li.dataset.cookieStoreId = identity.cookieStoreId
       ul.appendChild(li)
     }
     list.appendChild(ul)
   }
-})().then()
+
+  async handleClick({ cookieStoreId }) {
+    const [currentTab] = await browser.tabs.query({ currentWindow: true, active: true })
+    const { id, url, index, pinned } = currentTab
+
+    if (currentTab.cookieStoreId === cookieStoreId) return
+
+    const newTab = await browser.tabs.create({
+      url,
+      cookieStoreId,
+      index: index + 1,
+      pinned,
+    })
+    browser.tabs.remove(id)
+  }
+}
+
+const container = new ContainerPlus()
